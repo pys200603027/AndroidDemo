7da867ef3771211d0355f591de6cb87e2aee0173
pys200603027 <pys200603027@gmail.com>
2019-09-17 17:19:23 +0800

    [update]add between anim

---
+++ betweenanim/src/main/java/com/example/betweenanim/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.betweenanim;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.view.animation.ScaleAnimation;
+
+public class MainActivity extends AppCompatActivity {
+
+    View demo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        demo = findViewById(R.id.demo);
+
+        findViewById(android.R.id.content).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scaleAnimation();
+            }
+        });
+    }
+
+    private void scaleAnimation() {
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1f, 1.4f, 1f, 1.4f);
+        scaleAnimation.setDuration(500);
+        scaleAnimation.setFillAfter(true);
+        demo.startAnimation(scaleAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ScaleAnimation scaleAnimation = new ScaleAnimation(1.4f, 1f, 1.4f, 1f);
+                scaleAnimation.setDuration(500);
+                scaleAnimation.setFillAfter(true);
+                demo.startAnimation(scaleAnimation);
+            }
+        }, 1500);
+    }
+}
