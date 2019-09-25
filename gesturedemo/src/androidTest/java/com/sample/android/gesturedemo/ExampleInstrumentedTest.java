90c75b841e4d5bcbb498a9a33bf03280bdedd668
pys200603027 <pys200603027@gmail.com>
2019-09-25 16:34:43 +0800

    update to androidx

--- gesturedemo/src/androidTest/java/com/sample/android/gesturedemo/ExampleInstrumentedTest.java
+++ gesturedemo/src/androidTest/java/com/sample/android/gesturedemo/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
-package com.example.androiddemo;
+package com.sample.android.gesturedemo;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -19,8 +20,8 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.androiddemo", appContext.getPackageName());
+        assertEquals("com.sample.android.gesturedemo", appContext.getPackageName());
     }
 }
